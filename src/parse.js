10556b160f63e18ce2bfa7d5e2a226205b61a204
filipstefansson <filip.stefansson@gmail.com>
2018-03-04 17:41:09 +0000

    Added parse method and tests

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,73 @@
 /* @flow */
 
+type Param = {
+  key: string,
+  value: ?string,
+};
+
+type ParamsObject = {
+  [string]: ?string | (?string)[],
+};
+
+// TODO: move this to its own file
+const parseFormatter = (
+  key: string,
+  value: ?string,
+  accumulator: ParamsObject,
+): ?string | (?string)[] => {
+  const decodedValue: ?string = value ? decodeURIComponent(value) : null;
+  // if there's no previous value, just add it
+  if (accumulator[key] === undefined) return (accumulator[key] = decodedValue);
+  // if there's a previous value, add new value to that
+  return [].concat(accumulator[key], decodedValue);
+};
+
 /**
  * Parse a query string.
  *
  * @param  {string} string A query string
- * @return {Object}        The query string params
+ * @return {ParamsObject}        The query string params
  */
-export default (query: string): Object => {
-  return { foo: 'bar' };
+export default (query: string): ParamsObject => {
+  // early exit
+  if (!query || typeof query !== 'string') return (Object.create(null): any);
+
+  // remove spaces and any ?&# in the beginning of the string
+  const queryString: string = query.trim().replace(/^[?#&]/, '');
+
+  // split query in to an array of { key: ..., value: ...} objects
+  const params: Param[] = queryString
+    .split('&')
+    .filter(param => !!param)
+    .map((param: string): Param => {
+      const parts: string[] = param.split('=');
+      const key: string = parts.shift();
+      const value: ?string = parts.length > 0 ? parts.join('=') : null;
+      return { key: decodeURIComponent(key), value };
+    });
+
+  // reduce array in to an object we can return
+  // Object.create(null) creates a new object without prototype
+  const reduced: ParamsObject = params.reduce(
+    (paramsObj: ParamsObject, param: Param) => {
+      // copy previous params object
+      const newParamsObj: ParamsObject = Object.assign(
+        (Object.create(null): any),
+        paramsObj,
+      );
+
+      // pass key, value and current params object to the formatter
+      // TODO: add more formatters and support for custom function
+      newParamsObj[param.key] = parseFormatter(
+        param.key,
+        param.value,
+        newParamsObj,
+      );
+
+      return newParamsObj;
+    },
+    (Object.create(null): any),
+  );
+
+  return reduced;
 };
--- test/parse.test.js
+++ test/parse.test.js
@@ -8,4 +8,56 @@ describe('parse', () => {
   it('to return an object with matching values', () => {
     expect(parse('foo=bar')).toEqual({ foo: 'bar' });
   });
+
+  it('can parse multiple values', () => {
+    expect(parse('foo=bar&baz=qux')).toEqual({ foo: 'bar', baz: 'qux' });
+  });
+
+  it('can parse multiple values with same key', () => {
+    expect(parse('foo=bar&foo=baz')).toEqual({ foo: ['bar', 'baz'] });
+  });
+
+  it('preserves order of multiple keys', () => {
+    expect(parse('foo=bar&foo=baz')).toEqual({ foo: ['bar', 'baz'] });
+    expect(parse('foo=baz&foo=bar')).toEqual({ foo: ['baz', 'bar'] });
+  });
+
+  it('can parse string without a value', () => {
+    expect(parse('foo')).toEqual({ foo: null });
+    expect(parse('foo&bar')).toEqual({ foo: null, bar: null });
+    expect(parse('foo&bar=baz')).toEqual({ foo: null, bar: 'baz' });
+    expect(parse('foo&foo')).toEqual({ foo: [null, null] });
+  });
+
+  it('returns empty object if keys are missing', () => {
+    expect(parse()).toEqual({});
+    expect(parse('')).toEqual({});
+    expect(parse(' ')).toEqual({});
+    expect(parse('#')).toEqual({});
+    expect(parse('?')).toEqual({});
+  });
+
+  it('can parse strings starting with ?&#', () => {
+    expect(parse('?foo=bar')).toEqual({ foo: 'bar' });
+    expect(parse('#foo=bar')).toEqual({ foo: 'bar' });
+    expect(parse('&foo=bar')).toEqual({ foo: 'bar' });
+  });
+
+  it('returns empty object if input is not a string', () => {
+    expect(parse({})).toEqual({});
+  });
+
+  it('can handle embedded `=`', () => {
+    expect(parse('foo=bar%3Dbaz')).toEqual({ foo: 'bar=baz' });
+    expect(parse('foo=bar=baz')).toEqual({ foo: 'bar=baz' });
+  });
+
+  it('can handle keys that conflict with object properties', () => {
+    expect(parse('hasOwnProperty=foo')).toEqual({ hasOwnProperty: 'foo' });
+  });
+
+  it('can decode keys and values', () => {
+    expect(parse('f%C3%A5%C3%A5=bar')).toEqual({ fåå: 'bar' });
+    expect(parse('foo=b%C3%A5r')).toEqual({ foo: 'bår' });
+  });
 });
