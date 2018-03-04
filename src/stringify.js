9f53ff1fdd9882f78baee08719db87e0873e9932
filipstefansson <filip.stefansson@gmail.com>
2018-03-04 17:58:51 +0000

    Added basic stringify implementation

--- src/stringify.js
+++ src/stringify.js
@@ -6,6 +6,19 @@
  * @param  {Object} params Object with params
  * @return {string}        A query string
  */
-export default (params: Object): string => {
-  return 'foo=bar';
+export default (params: ?Object): string => {
+  if (!params) return '';
+
+  // create new params object
+  const paramsObject: Object = Object.assign({}, params);
+
+  // go through params and create array of key=value strings
+  const paramsArray: string[] = Object.keys(paramsObject).map((key: string) => {
+    return `${key}=${paramsObject[key]}`;
+  });
+
+  // join params with and &
+  const joinedParams = paramsArray.join('&');
+
+  return joinedParams;
 };
--- test/stringify.test.js
+++ test/stringify.test.js
@@ -5,7 +5,11 @@ describe('stringify', () => {
     expect(stringify).toBeDefined();
   });
 
-  it('to return a string with matching values', () => {
+  it('should return a string with matching values', () => {
     expect(stringify({ foo: 'bar' })).toEqual('foo=bar');
+  });
+
+  it('it should return a string with multiple values', () => {
+    expect(stringify({ foo: 'bar', baz: 'qux' })).toEqual('foo=bar&baz=qux');
   });
 });
